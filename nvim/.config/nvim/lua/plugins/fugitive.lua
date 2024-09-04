e8a1ffb0344c43fb02cfc5c89064c431bbd59e98
Saminjay Goel <32176560+saminjay@users.noreply.github.com>
2024-09-04 22:30:56 +0530

    minor changes

--- nvim/.config/nvim/lua/plugins/fugitive.lua
+++ nvim/.config/nvim/lua/plugins/fugitive.lua
@@ -1,2 +1,8 @@
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-return { "tpope/vim-fugitive" }
+return {
+    {
+        "tpope/vim-fugitive",
+        config = function ()
+            vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+        end,
+    }
+}
--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,12 +1,3 @@
-local builtin = require('telescope.builtin')
-local telescopeConfig = require("telescope.config")
-
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>h', builtin.help_tags, {})
-
 return {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.8',
@@ -15,7 +6,13 @@ return {
         defaults = {
             -- `hidden = true` is not supported in text grep commands.
             vimgrep_arguments = {
-                unpack(telescopeConfig.values.vimgrep_arguments), -- default config
+                "--color=never",
+                "--no-heading",
+                "--with-filename",
+                "--line-number",
+                "--column",
+                "--smart-case",
+                -- added argumnets to show hidden files
                 "--hidden",
                 "--glob",
                 "!**/.git/*",
@@ -28,4 +25,12 @@ return {
             },
         },
     },
+    config = function ()
+        local builtin = require('telescope.builtin')
+        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+        vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
+        vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
+        vim.keymap.set('n', '<leader>h', builtin.help_tags, {})
+    end
 }
