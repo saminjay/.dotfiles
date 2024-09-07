c2254327e5bde443713eb5d2eaa67ef00a84ddba
Saminjay Goel <32176560+saminjay@users.noreply.github.com>
2024-09-08 02:02:17 +0530

    refactor telescope

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,36 +1,49 @@
+local opts = function ()
+    local builtin = require("telescope.builtin")
+
+    -- preferred remaps
+    vim.keymap.set("n", "<leader>pf", builtin.find_files, {});
+    vim.keymap.set("n", "<C-p>", builtin.git_files, {});
+    vim.keymap.set("n", "<leader>ps", builtin.live_grep, {});
+    vim.keymap.set("n", "<leader>pb", builtin.buffers, {});
+    vim.keymap.set("n", "<leader>h", builtin.help_tags, {});
+    vim.keymap.set("n", "<leader>ts", builtin.treesitter, {});
+
+    local defaults = {
+        vimgrep_arguments = {
+            "--color=never",
+            "--no-heading",
+            "--with-filename",
+            "--line-number",
+            "--column",
+            "--smart-case",
+            "--hidden",     -- show hidden files
+            "--glob",       -- hide .git files
+            "!**/.git/*",
+        },
+    };
+
+    local pickers = {
+        find_files = {
+            find_command = {
+                "rg",
+                "--files",
+                "--hidden",     -- show hidden files
+                "--glob",       -- hide .git files
+                "!**/.git/*",
+            },
+        },
+    };
+    return {
+        defaults = defaults,
+        pickers = pickers,
+    };
+end;
+
 return {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.8',
     dependencies = { 'nvim-lua/plenary.nvim' },
-    opts = {
-        defaults = {
-            -- `hidden = true` is not supported in text grep commands.
-            vimgrep_arguments = {
-                "--color=never",
-                "--no-heading",
-                "--with-filename",
-                "--line-number",
-                "--column",
-                "--smart-case",
-                -- added argumnets to show hidden files
-                "--hidden",
-                "--glob",
-                "!**/.git/*",
-            },
-        },
-        pickers = {
-            find_files = {
-                -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
-                find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
-            },
-        },
-    },
-    config = function ()
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
-        vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
-        vim.keymap.set('n', '<leader>h', builtin.help_tags, {})
-    end
-}
+    opts = opts,
+};
+
