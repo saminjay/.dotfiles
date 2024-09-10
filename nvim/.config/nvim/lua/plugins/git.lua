11209411a49e91e6f263a16f6b75ef8efd4f3c43
Saminjay Goel <32176560+saminjay@users.noreply.github.com>
2024-09-10 18:23:20 +0530

    added git signs support

---
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -0,0 +1,12 @@
+return {
+    {
+        "tpope/vim-fugitive",
+        config = function ()
+            vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+        end,
+    },
+    {
+        'lewis6991/gitsigns.nvim',
+        opts = {},
+    },
+}
--- nvim/.config/nvim/lua/plugins/helpers.lua
+++ nvim/.config/nvim/lua/plugins/helpers.lua
@@ -1,10 +1,9 @@
 local todo_comments = {
     "folke/todo-comments.nvim",
+    event = "VimEnter",
     dependencies = { "nvim-lua/plenary.nvim" },
     opts = {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
+        signs = false,
     },
 }
 
