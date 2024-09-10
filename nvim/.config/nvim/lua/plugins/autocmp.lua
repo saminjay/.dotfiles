ca1c3020b0253b33a0e3eca849d8223d3dc6ede2
Saminjay Goel <32176560+saminjay@users.noreply.github.com>
2024-09-11 00:06:29 +0530

    mapping tweaks for nvim-cmp

--- nvim/.config/nvim/lua/plugins/autocmp.lua
+++ nvim/.config/nvim/lua/plugins/autocmp.lua
@@ -26,18 +26,16 @@ return {
                 mapping = {
                     ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
                     ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-
-                    -- TODO: test these functions and find a new keymap
-                    ["<CR>"] = cmp.mapping.confirm({ select = true }),  -- can use <C-y>
-                    ["<C-y>"] = cmp.mapping.complete(), -- can use <C-space>
+                    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+                    ['<C-e>'] = cmp.mapping.abort(),
+                    ["<C-y>"] = cmp.mapping.complete(),
                 },
                 sources = cmp.config.sources({
                     { name = "nvim_lsp" },
                     { name = "luasnip" }, -- For luasnip users.
                     {
                         name = "lazydev",
-                        -- set group index to 0 to skip loading LuaLS completions as lazydev recommends it
-                        group_index = 0,
+                        group_index = 0,    -- Use first
                     },
                     { name = "path" },
                 }, {
