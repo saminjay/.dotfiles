c650329c1c9820dce3925d0240ebaf86af493d59
Saminjay Goel <32176560+saminjay@users.noreply.github.com>
2024-10-10 07:40:26 +0530

    minor config fix

--- nvim/.config/nvim/lua/config/remap.lua
+++ nvim/.config/nvim/lua/config/remap.lua
@@ -22,7 +22,7 @@ vim.keymap.set("x", "<leader>p", [["_dP]])
 -- next greatest remap ever : asbjornHaland
 -- yanks to the + register
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set("n", "<leader>Y", [["+y$]]) -- 'Y' is remapped to 'y$' in nvim
 
 -- deletes to the void register
 vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
