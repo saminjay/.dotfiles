dc8db2d7edfa8ede4712a88a390b59b58d8e4361
Saminjay Goel <32176560+saminjay@users.noreply.github.com>
2024-10-15 08:51:06 +0530

    added hardtime and highligh_color plugins

--- dot-config/nvim/lua/plugins/helpers.lua
+++ dot-config/nvim/lua/plugins/helpers.lua
@@ -7,6 +7,19 @@ local todo_comments = {
     },
 }
 
+local highlight_colors = {
+    "brenoprata10/nvim-highlight-colors",
+    opts = {},
+}
+
+-- TODO: remove after getting comfortable
+local hardtime = {
+   "m4xshen/hardtime.nvim",
+    event = "BufRead",
+   dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+   opts = {}
+};
+
 local which_key = {
     "folke/which-key.nvim",
     event = "VeryLazy",
@@ -28,5 +41,8 @@ local which_key = {
 
 return {
     todo_comments,
+    highlight_colors,
+    hardtime,
     -- which_key,   -- FIXIT: doesn't open properly in files
 }
+
