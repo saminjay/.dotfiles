fef7cb71262ed1791b5f9fe4081abd029568b6b2
Saminjay Goel <32176560+saminjay@users.noreply.github.com>
2024-11-14 00:06:54 +0530

    minor additions

--- install.sh
+++ install.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
 # install all required packages
-sudo pacman -S - < packages.txt
+# shellcheck disable=SC2024
+sudo pacman -S - <packages.txt
 
 # Tmux Plugin Manager
-git clone --depth=1 https://github.com/tmux-plugins/tpm ${XDG_DATA_HOME}/tmux/plugins/tpm
+git clone --depth=1 https://github.com/tmux-plugins/tpm "${XDG_DATA_HOME}/tmux/plugins/tpm"
 
 # Powerlevel10k theme for zsh
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${XDG_DATA_HOME}/powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${XDG_DATA_HOME}/powerlevel10k"
 
 # TODO:
 # Change shell to zsh
@@ -17,4 +18,3 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${XDG_DATA_HOME
 ln -s /dev/null /etc/udev/rules.d/61-gdm.rules
 
 stow --dotfiles .
-
