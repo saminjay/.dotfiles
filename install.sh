fb2201055ffa050b6f84c25edd88ab15387caaca
Saminjay Goel <32176560+saminjay@users.noreply.github.com>
2025-01-04 16:27:49 +0530

    install.sh bug fix

--- install.sh
+++ install.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/env bash
 
-# install all required packages
 # shellcheck disable=SC2024
+# install all required packages
 sudo pacman -S - <packages.txt
 
 # Tmux Plugin Manager
 git clone --depth=1 https://github.com/tmux-plugins/tpm "${XDG_DATA_HOME}/tmux/plugins/tpm"
 
 # Powerlevel10k theme for zsh
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${XDG_DATA_HOME}/powerlevel10k"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${XDG_DATA_HOME}/zsh/plugins/powerlevel10k"
 
-# TODO:
-# Change shell to zsh
-# sudo chsh -s $(which zsh)
+# Change login shell to zsh
+chsh -s "$(which zsh)"
 
 # Force gdm to use wayland
 ln -s /dev/null /etc/udev/rules.d/61-gdm.rules
 
-stow --dotfiles .
+stow .
