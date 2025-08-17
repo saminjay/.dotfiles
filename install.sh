2ee024e11502d10f12acfc712238990ce371f604
Saminjay Goel <32176560+saminjay@users.noreply.github.com>
2025-08-18 02:40:04 +0530

    updates and fixes

--- install.sh
+++ install.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
 
+home="/home/neo"
+
 # shellcheck disable=SC2024
 # install all required packages
 sudo pacman -S - <packages.txt
 
 # Tmux Plugin Manager
-git clone --depth=1 https://github.com/tmux-plugins/tpm "${XDG_DATA_HOME}/tmux/plugins/tpm"
+git clone --depth=1 https://github.com/tmux-plugins/tpm "$home/.local/share/tmux/plugins/tpm"
 
 # Change login shell to zsh
 chsh -s "$(which zsh)"
 
 # Force gdm to use wayland
-ln -s /dev/null /etc/udev/rules.d/61-gdm.rules
+# ln -s /dev/null /etc/udev/rules.d/61-gdm.rules
 
 stow .
+
+sudo cp -ri ./etc/* /etc/
